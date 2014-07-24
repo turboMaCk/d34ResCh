fed79290bb874c8c915c891b041a440df58ed2d4
Marek Fajkus <marek.faj@gmail.com>
2014-07-25 00:25:22 +0200

    Improve pie chart

--- src/pie-chart.js
+++ src/pie-chart.js
@@ -20,13 +20,14 @@ pieChart.prototype = {
     // duration of animations
     this.duration = 750;
 
-    this.outerRadius = 70;
-    this.innerRadius = 50;
+    this.thickness = 15;
 
     // first setup
     this.setupDimensions();
 
     this.setupChart();
+
+    this.redrawChart(this.currentData);
 
     return this;
   },
@@ -36,26 +37,15 @@ pieChart.prototype = {
    * @param data [data - array of objects]
    */
   parse_data: function(data) {
+    this.currentData = data;
     return data;
-  },
-  /**
-   * Arc
-   * @arg start [int] - radians
-   * @arg end [int] - radians
-   */
-  arc: function(start, end) {
-    var start = 45 * (Math.PI/180) * start
-    return d3.svg.arc()
-      .innerRadius(this.innerRadius)
-      .outerRadius(this.outerRadius)
-      .startAngle(start)
-      .endAngle(end);
   },
   /**
    * Setup dimensions
    * @desc Setup charts dimensions
    */
   setupDimensions: function() {
+    var self = this;
 
     // dimensions of whole svg
     this.outerWidth = parseInt(this.container.style('width'));
@@ -64,10 +54,23 @@ pieChart.prototype = {
     // setup dimensions of chart
     this.width = this.outerWidth - this.margin.left - this.margin.right;
     this.height = this.outerHeight - this.margin.top - this.margin.bottom;
+    this.radius = Math.min(this.width, this.height) / 2;
 
+    this.outerRadius = Math.min(self.width, self.height) / 2;
+    this.innerRadius = self.outerRadius - this.thickness;
+
+    // setup pie parts
     this.arc = d3.svg.arc()
-      .outerRadius();
+      .outerRadius(self.outerRadius)
+      .innerRadius(self.innerRadius);
 
+    this.pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) {
+        return d.value;
+      });
+
+    return this;
   },
   /**
    * Setup Chart
@@ -75,30 +78,72 @@ pieChart.prototype = {
    * @return instace of this
    */
   setupChart: function() {
-    //var self = this;
 
     // define svg
     this.svg = this.container
       .append('svg')
-      .attr('class', 'pie-chart')
+      .attr('class', 'pie-chart');
+
+    this.chart = this.svg
       .append('g')
       .attr('transform', 'translate(' + this.outerWidth/2 + ',' + this.outerHeight/2 + ')');
+
+    this.legend = this.svg
+      .append('g')
+      .attr('class', 'legend')
+      .attr('transform', 'translate(' + this.outerHeight / 2 + ',' + this.height + ')');
 
     return this;
   },
   redrawChart: function(newData) {
     var self = this;
     var svg = this.svg;
-    var duration = this.duration;
+
+    this.color = d3.scale.ordinal()
+      .range(["#3FB0CC", "#A1D8E6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    svg
+      .style('height', self.outerHeight)
+      .style('width', self.outerWidth);
 
     // parse data
-    var data = newData ? this.parse_data(data) : this.currentData;
+    var data = newData ? this.parse_data(newData) : this.currentData;
 
     // stop if there is no data
     if (!data) return false;
 
-    data.forEach(function(d) {
-      self.svg.append('path').call(self.arc(1,d.value));
-    });
+    this.drawLegend(data);
+
+    // define arcs
+    var arcs = this.chart.selectAll('.arc')
+      .data(self.pie(data))
+      .each(function(d) { this._current = d; });
+
+    // enter
+    arcs.enter().append('g')
+      .attr('class', 'arc');
+
+    arcs.transition().duration(750);
+
+    arcs.append('path')
+      .attr('d', self.arc)
+      .style('fill', function(d) {
+        return self.color(d.data.value);
+      });
+  },
+  drawLegend: function(data) {
+    var self = this;
+
+    var items = this.legend.selectAll('.item')
+      .data(data);
+
+    items.enter().append('g')
+      .attr('class', '.item')
+      .append('circle')
+      .attr('r', 4)
+      .style('fill', function(d) {
+        console.log(d);
+        return self.color(d.value);
+      });
   }
 };
